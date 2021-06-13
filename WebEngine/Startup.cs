c6ec712f529d756be0853070bcd1586487723223
danilov.fedor <danilov.fedor@otr.ru>
2021-06-13 16:08:40 +0300

    Модель "Меню" предварительный

--- WebEngine/Startup.cs
+++ WebEngine/Startup.cs
@@ -12,14 +12,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebEngine.Data;
+using WebEngine.Interfaces.Menus;
+using WebEngine.Repository.Menus;
 
 namespace WebEngine
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IConfigurationRoot _connString;
+
+        public Startup(IConfiguration configuration, Microsoft.AspNetCore.Hosting.IHostingEnvironment hostEnv)
         {
             Configuration = configuration;
+            _connString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -27,14 +32,15 @@ namespace WebEngine
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(_connString.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddMvcCore(option => option.EnableEndpointRouting = false);
+
+            services.AddTransient<IMenuItem, MenuItemRepository>();
+            services.AddTransient<IMenu, MenuRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
