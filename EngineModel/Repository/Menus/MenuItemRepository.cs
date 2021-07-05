6e83261a8d138a94f62bd2c3544408b63afaad10
danilov.fedor <danilov.fedor@otr.ru>
2021-07-05 18:55:50 +0300

    Перевод проекта на Blazor

---
+++ EngineModel/Repository/Menus/MenuItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using EngineModel.Models.Menus;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WebEngine.Interfaces.Menus;
+
+namespace EngineModel.Repository.Menus
+{
+    public class MenuItemRepository : IMenuItem
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public MenuItemRepository(ApplicationDbContext applicationDbContext) {
+            this.applicationDbContext = applicationDbContext;
+        }
+        //Необходимо отладить
+        public IEnumerable<MenuItem> AllMenuItemsById(int id) => applicationDbContext.MenuItem.Where(p => p.MenuId == id).Include(c => c.Menu);
+
+        public MenuItem MenuItem(int id) => applicationDbContext.MenuItem.FirstOrDefault(p => p.Id == id);
+    }
+}
