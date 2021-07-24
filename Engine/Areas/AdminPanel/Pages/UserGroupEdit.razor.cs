9332a12e8bd5adc607baf0c7a6e680e2d306092a
danilov.fedor <danilov.fedor@otr.ru>
2021-07-24 20:39:54 +0300

    close #100 AdminPanel: Back. Пользователи -> Группы пользователей -> Изменить

---
+++ Engine/Areas/AdminPanel/Pages/UserGroupEdit.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Engine.Areas.AdminPanel.Pages
+{
+    public partial class UserGroupEdit
+    {
+        [Parameter]
+        public string GroupId { get; set; }
+        [Inject]
+        private RoleManager<IdentityRole> roleManager { get; set; }
+        [Inject]
+        private NavigationManager Nav { get; set; }
+        private string _groupId;
+        private IdentityRole editedRole = null;
+        protected override async Task OnInitializedAsync() {
+            _groupId = GroupId;
+            editedRole = await roleManager.FindByIdAsync(_groupId);
+            await base.OnInitializedAsync();
+        }
+
+        private async Task UpdateRole() {
+            await roleManager.UpdateAsync(editedRole);
+            Cancel();
+        }
+
+        private void Cancel() {
+            Nav.NavigateTo($"/administrator/user/groups");
+        }
+    }
+}
