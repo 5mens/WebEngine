f9c4377a942855067d57ff162a6efbe9c27289a2
danilov.fedor <danilov.fedor@otr.ru>
2021-07-25 00:19:20 +0300

    close #62 Логика: Изменить меню

---
+++ Engine/Areas/AdminPanel/Pages/MenuAdd.razor.cs
@@ -0,0 +1,61 @@
+﻿using Engine.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Engine.Models.Interfaces;
+using MudBlazor;
+using Engine.Models.BaseClasses;
+
+namespace Engine.Areas.AdminPanel.Pages
+{
+    public partial class MenuAdd
+    {
+        [Inject]
+        private IMenu MyMenu { get; set; }
+        [Inject]
+        private NavigationManager Nav { get; set; }
+        [Inject]
+        private ISnackbar Snackbar { get; set; }
+        private Menu NewMenu;
+
+        private string[] _selectorValue = { "main", "top", "left", "right", "bottom", "footer" };
+        protected override async Task OnInitializedAsync()
+        {
+            NewMenu = new();
+            NewMenu.Title = string.Empty;
+            NewMenu.Menutype = string.Empty;
+            NewMenu.Published = false;
+            await base.OnInitializedAsync();
+        }
+        /// <summary>
+        /// Сохранить
+        /// </summary>
+        private async Task OnButtonClicked()
+        {            
+            if ((NewMenu.Title != string.Empty || NewMenu.Title != "") && (NewMenu.Menutype != string.Empty || NewMenu.Menutype != ""))
+            {                
+                await MyMenu.AddNewMenu(NewMenu);
+                Snackbar.Add("Меню добавлено", Severity.Success);
+                Cancel();
+            }
+            else if (NewMenu.Title != string.Empty || NewMenu.Title != "")
+            {
+                Snackbar.Add("Необходимо заполнить наименование меню", Severity.Error);
+            }
+            else
+            {
+                Snackbar.Add("Выберите положение меню", Severity.Error);
+            }
+        }
+
+        /// <summary>
+        /// Отмена
+        /// </summary>
+        private void Cancel()
+        {
+            Nav.NavigateTo($"/administrator/menu/list");
+        }
+    }
+}
---
+++ Engine/Areas/AdminPanel/Pages/MenuEdit.razor.cs
@@ -0,0 +1,67 @@
+﻿using Engine.Models.BaseClasses;
+using Engine.Models.Interfaces;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Engine.Areas.AdminPanel.Pages
+{
+    public partial class MenuEdit
+    {
+        [Parameter]
+        public int MenuId { get; set; }
+        [Inject]
+        private IMenu MyMenu { get; set; }
+        [Inject]
+        private NavigationManager Nav { get; set; }
+        [Inject]
+        private ISnackbar Snackbar { get; set; }
+        private bool Busy;
+        private Menu EditedItem;
+        private string[] _selectorValue = { "main", "top", "left", "right", "bottom", "footer" };
+        protected override async Task OnInitializedAsync()
+        {
+            Busy = true;
+            try
+            {
+                EditedItem = await MyMenu.GetMenu(MenuId);
+            }
+            finally
+            {
+                Busy = false;
+            }
+            await base.OnInitializedAsync();
+        }
+
+        /// <summary>
+        /// Сохранить
+        /// </summary>
+        private async Task UpdateMenu()
+        {
+            if ((EditedItem.Title != string.Empty || EditedItem.Title != "") && (EditedItem.Menutype != string.Empty || EditedItem.Menutype != ""))
+            {
+                await MyMenu.UpdateMenu(EditedItem);
+                Snackbar.Add("Меню изменено", Severity.Success);
+                Cancel();
+            }
+            else if (EditedItem.Title != string.Empty || EditedItem.Title != "")
+            {
+                Snackbar.Add("Необходимо заполнить наименование меню", Severity.Error);
+            }
+            else
+            {
+                Snackbar.Add("Выберите положение меню", Severity.Error);
+            }
+        }
+        /// <summary>
+        /// Отмена
+        /// </summary>
+        private void Cancel()
+        {
+            Nav.NavigateTo($"/administrator/menu/list");
+        }
+    }
+}
