86fe1cb5d146f6f2a86fe9fe25d53bfa7bf56780
danilov.fedor <danilov.fedor@otr.ru>
2021-07-26 22:49:13 +0300

    close #121 Автоматический вход в панель управления (при активной авторизации)

---
+++ Engine/Areas/WebSite/Shared/Login.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Http;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Engine.Areas.WebSite.Shared
+{
+    public partial class Login
+    {
+        [Inject]
+        NavigationManager Nav { get; set; }
+
+        [CascadingParameter] protected Task<AuthenticationState> AuthStat { get; set; }
+        protected async override Task OnInitializedAsync()
+        {            
+            base.OnInitialized();
+        }
+    }
+}
