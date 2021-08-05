f1fc87f7e2d2c5b717a43e06142a5ef35e6a8279
zakharov.nikolay <nikalezah@gmail.com>
2021-08-05 19:39:28 +0300

    resolves #152 AdminPanel: Back Справочники -> Ингредиенты

---
+++ Engine/Models/Interfaces/IIngredientCategory.cs
@@ -0,0 +1,15 @@
+﻿using Engine.Models.BaseClasses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Engine.Models.Interfaces
+{
+    public interface IIngredientCategory
+    {
+        /// <summary>
+        /// Возвращает все категории ингредиентов
+        /// </summary>
+        /// <returns>Список категорий ингредиентов</returns>
+        Task<List<IngredientCategory>> GetAll();
+    }
+}
---
+++ Engine/Models/Repository/IngredientCategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Engine.Data;
+using Engine.Models.BaseClasses;
+using Engine.Models.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Engine.Models.Repository
+{
+    public class IngredientCategoryRepository : IIngredientCategory
+    {
+        private IDbContextFactory<AppDbContext> DbFactory { get; set; }
+
+        public IngredientCategoryRepository(IDbContextFactory<AppDbContext> contextFactory)
+        {
+            DbFactory = contextFactory;
+        }
+
+        public async Task<List<IngredientCategory>> GetAll()
+        {
+            return await DbFactory.CreateDbContext().IngredientCategory.ToListAsync();
+        }
+    }
+}
