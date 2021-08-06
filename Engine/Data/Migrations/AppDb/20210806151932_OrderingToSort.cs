67d59e7e66b55fb209721b3674e9f999e96f6e23
danilov.fedor <danilov.fedor@otr.ru>
2021-08-06 18:24:11 +0300

    close #168 БД Корректировка колонок OrderingToSort

---
+++ Engine/Data/Migrations/AppDb/20210806151932_OrderingToSort.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Engine.Data.Migrations.AppDb
+{
+    public partial class OrderingToSort : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Ordering",
+                table: "MenuItem",
+                newName: "sort");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "sort",
+                table: "MenuItem",
+                newName: "Ordering");
+        }
+    }
+}
